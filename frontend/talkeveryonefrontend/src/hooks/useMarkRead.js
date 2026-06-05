2cb3030177b6442b7f10c425e16d8c2b7c165880
ayush kumar <ayushkumar19999@gmail.com>
2026-06-05 22:47:40 +0530

    optimization check 4

--- frontend/talkeveryonefrontend/src/hooks/useMarkRead.js
+++ frontend/talkeveryonefrontend/src/hooks/useMarkRead.js
@@ -1,14 +1,12 @@
 // src/hooks/useMarkRead.js
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation} from "@tanstack/react-query";
 import { markMessagesRead } from "@/api/chatApi";
 
 export const useMarkRead = () => {
-    const queryClient = useQueryClient();
 
     return useMutation({
-        mutationFn: (chatId) => markMessagesRead(chatId), // ← chatId yahan se aayega
-        onSuccess: (_, chatId) => { // ← variables mein chatId milega
-            queryClient.invalidateQueries({ queryKey: ["chats"] });
-        }
+        mutationFn: (chatId) =>
+            markMessagesRead(chatId)
     });
+
 };
