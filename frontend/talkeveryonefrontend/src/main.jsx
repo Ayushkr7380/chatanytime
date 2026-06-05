3ca7311f28ae1acc3e582fefa90b468e6978c898
ayush kumar <ayushkumar19999@gmail.com>
2026-06-05 17:16:38 +0530

    fix: resolve case-sensitive import path

--- frontend/talkeveryonefrontend/src/main.jsx
+++ frontend/talkeveryonefrontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 // import UserContext from './context/userContext/userContext.jsx'
-import SocketContext from './context/socketContext/socketContext.jsx'
+import SocketContext from './context/socketContext/SocketContext.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
