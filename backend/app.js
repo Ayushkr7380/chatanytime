010fb34f441660b09d35422de9c471af346672c0
ayush kumar <ayushkumar19999@gmail.com>
2026-06-05 17:34:03 +0530

    fix: update production cors and cookie configuration

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,10 @@
-import {config} from "dotenv";
+import { config } from "dotenv";
 config();
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import express from "express";
 import { createServer } from "http";
-import {Server } from "socket.io";
+import { Server } from "socket.io";
 import cors from "cors";
 import userRouter from "./routes/userRoutes.js";
 import chatRouter from "./routes/chatRoutes.js";
@@ -16,32 +16,33 @@ const server = createServer(app);
 
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(cookieParser());
 
 const corsOption = {
-    origin : process.env.FRONTEND_URL,
-    credentials:true,
-    optionsSuccessStatus:200,
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    optionsSuccessStatus: 200,
 
 }
 
 app.use(cors(corsOption));
 
-const io = new Server(server,{
-    cors:{
-        origin:"http://localhost:5173"
+const io = new Server(server, {
+    cors: {
+        origin: process.env.FRONTEND_URL,
+        credentials: true,
     }
 });
 
 
-io.on("connection", async(socket) => {
+io.on("connection", async (socket) => {
     const userId = socket.handshake.auth?.userId;
 
     console.log("A new user connected:", socket.handshake.auth?.name);
 
-    
+
     if (userId) {
         socket.join(userId);
         console.log("User personal room joined:", userId);
@@ -58,27 +59,27 @@ io.on("connection", async(socket) => {
         });
     }
 
-   
+
     socket.on("joinChat", (chatId) => {
         socket.join(chatId);
         console.log("Room joined:", chatId);
     });
 
-   
+
     socket.on("sendMessage", async (msgInfo) => {
         console.log("Message received from frontend:", msgInfo);
 
-        
+
         const chat = await Chat.findById(msgInfo.chatId).select("users");
 
-      
+
         chat.users.forEach((user) => {
             io.to(user._id.toString()).emit("newMessageNotification", {
                 chatId: msgInfo.chatId,
             });
         });
 
-        
+
         io.to(msgInfo.chatId).emit("receiveMessage", {
             _id: Date.now(),
             content: msgInfo.content,
@@ -89,50 +90,50 @@ io.on("connection", async(socket) => {
             createdAt: new Date(),
         });
 
-        
+
     });
 
     socket.on("disconnect", async () => {
-            if (userId) {
-                await User.findByIdAndUpdate(userId, {
-                    isOnline: false,
-                    lastSeen: new Date()
-                });
-                console.log("User disconnected:", userId);
-            }
-
-             io.emit("userStatusUpdate", {
-                userId,
+        if (userId) {
+            await User.findByIdAndUpdate(userId, {
                 isOnline: false,
                 lastSeen: new Date()
             });
-
             console.log("User disconnected:", userId);
+        }
+
+        io.emit("userStatusUpdate", {
+            userId,
+            isOnline: false,
+            lastSeen: new Date()
+        });
+
+        console.log("User disconnected:", userId);
     });
 
 
     socket.on("typing", async ({ chatId }) => {
 
-    const user = await User.findById(userId)
-        .select("name");
+        const user = await User.findById(userId)
+            .select("name");
 
-    socket.to(chatId).emit(
-        "typing",
-        {
-            chatId,
-            userId,
-            name: user?.name
-        }
-    );
-});
+        socket.to(chatId).emit(
+            "typing",
+            {
+                chatId,
+                userId,
+                name: user?.name
+            }
+        );
+    });
 
     socket.on("stopTyping", ({ chatId }) => {
         socket.to(chatId).emit("stopTyping", { chatId });
     });
 });
 
-app.use("/auth",userRouter);
-app.use("/user",chatRouter);
+app.use("/auth", userRouter);
+app.use("/user", chatRouter);
 
 export { io };
 export default server;
